6291119bda35618f3b3384e4f4e4d0bfcab868ec
qusong <qusongdlzs@163.com>
2012-05-11 16:49:10 +0800

    第一次提交

--- class/Localizer.php
+++ class/Localizer.php
@@ -1,6 +1,8 @@
 <?php 
 namespace org\opencomb\localizer ;
 
+use org\jecat\framework\bean\BeanFactory;
+use org\opencomb\platform\mvc\view\widget\Menu;
 use org\jecat\framework\locale\LanguagePackageFolders;
 use org\jecat\framework\fs\Folder;
 use org\jecat\framework\locale\Locale;
@@ -10,8 +12,16 @@ class Localizer extends Extension
 {
 	public function load()
 	{
+		BeanFactory::singleton()->registerBeanClass("org\\opencomb\\localizer\\LangSelect",'langselect') ;
+		Menu::registerBuildHandle(
+				'org\\opencomb\\coresystem\\mvc\\controller\\ControlPanelFrame'
+				, 'frameView'
+				, 'mainMenu'
+				, array(__CLASS__,'buildControlPanelMenu')
+		) ;
 		// 注册语言包目录
 		LanguagePackageFolders::singleton()->registerFolder($this->unarchiveSentenceFolder()->path()) ;
+		
 	}
 	
 	/**
@@ -39,5 +49,27 @@ class Localizer extends Extension
 	public function unarchiveSentenceFolder()
 	{
 		return $this->dataFolder()->findFolder('lang',Folder::FIND_AUTO_CREATE) ;
+	}
+	
+	static public function buildControlPanelMenu(array & $arrConfig)
+	{
+		// 合并配置数组，增加菜单
+		$arrConfig['item:system']['item:platform-manage']['item:localizer'] = array(
+				'title'=> '本地化' ,
+				'link' => '?c=org.opencomb.localizer.LangSetting' ,
+				'query' => 'c=org.opencomb.localizer.LangSetting' ,
+				'menu' => 1,
+				'item:langsetting' => array(
+					'title' => '语言设定' ,
+					'link' => '?c=org.opencomb.localizer.LangSetting' ,	
+					'query' => 'c=org.opencomb.localizer.LangSetting' ,
+				),
+				'item:translation' => array(
+					'title' => '语言翻译' ,
+					'link' => '?c=org.opencomb.localizer.LangTranslation' ,
+					'query' => 'c=org.opencomb.localizer.LangTranslation' ,
+				)
+				
+		);
 	}
 }
