71221dfff7cd5e023405c4a3eb5825e858d68b36
qusong <qusongdlzs@163.com>
2012-07-11 10:03:24 +0800

    add search

---
+++ class/LangSearchKey.php
@@ -0,0 +1,429 @@
+<?php
+namespace org\opencomb\localizer;
+use org\jecat\framework\mvc\view\UIFactory;
+
+use org\jecat\framework\verifier\Length;
+
+use org\jecat\framework\lang\Object;
+use org\jecat\framework\message\Message;
+use org\jecat\framework\lang\oop\ClassLoader;
+use org\jecat\framework\mvc\view\widget\menu\Menu;
+use org\jecat\framework\mvc\view\View;
+use org\jecat\framework\mvc\controller\Controller;
+use org\jecat\framework\lang\aop\AOP;
+use org\opencomb\platform\ext\Extension;
+use org\jecat\framework\bean\BeanFactory;
+use org\opencomb\frameworktest\aspect;
+use org\opencomb\platform\system\PlatformSerializer;
+use org\jecat\framework\ui\xhtml\weave\Patch;
+use org\jecat\framework\ui\xhtml\weave\WeaveManager;
+use org\opencomb\coresystem\mvc\controller\ControlPanel;
+use org\opencomb\coresystem\mvc\controller\ControlPanelFrame;
+use org\jecat\framework\locale\Locale;
+use org\jecat\framework\locale\SentenceLibrary;
+use org\jecat\framework\setting\Setting;
+
+class LangSearchKey extends ControlPanel
+{
+	protected $arrConfig = array(
+				'title'=> '本地化翻译',
+				'view'=>array(
+					'template'=>'LangSearchKey.html',
+					'class'=>'view',
+					'widget:paginator' => array(
+							'class' => 'paginator' ,
+					) ,
+				),
+		);
+
+	
+	public function process()
+	{
+		$sSearchLangCountry = $this->params['sLangCountry'];
+		$sSearchKey = $this->params['searchkey'];
+		$this->view->variables()->set('hidelangcountry', $sSearchLangCountry);
+		if(empty($sSearchKey))
+		{
+			$this->createMessage(Message::error,"%s",$key="请输入搜索内容");
+			$arrLangSelectMenu = $this->getLangSelectMenu();
+			$this->view->variables()->set('arrLangSelectMenu',$arrLangSelectMenu);
+			return;
+		}
+		$arrSentenceLibrary = $this->getSelectSentenceLibrary($sSearchLangCountry);
+		$arrSentenceLibrarySeach = $this->searchKey($arrSentenceLibrary,$sSearchKey);//var_dump($arrSentenceLibrarySeach);exit;
+		$arrLangTranslationSelect = $this->setSelectSentenceLibraryPage(null,$arrSentenceLibrarySeach);
+		//$this->view->variables()->set('hidelangcountry', $this->params['sSwichFrontLangPath']);
+		
+		$sLangCountry = $sSearchLangCountry;
+		if(count($arrLangTranslationSelect)==0)
+		{
+			$bFlag = false;
+			$this->view->variables()->set('bFlag',$bFlag);
+		
+			$this->view->variables()->set('sSpath',$sLangCountry);
+			$arrLangSelectMenu = $this->getLangSelectMenu();
+			$this->view->variables()->set('arrLangSelectMenu',$arrLangSelectMenu);
+			$this->view->variables()->set('bFlagSearch',true);
+		}else{
+		
+			$bFlag = true;
+			$this->view->variables()->set('bFlag',$bFlag);
+		
+			$arrLangTranslationChunk = array();
+			$arrLangTranslationChunk = $this->getLangChunk($arrLangTranslationSelect,$nPerPageRowNumber=20);
+			$arrLangTranslationNew = $this->getSelectSentenceLibraryNew($sLangCountry, $arrLangTranslationChunk,1);
+		
+			$this->setPaginator($arrSentenceLibrarySeach, $sLangCountry);
+		
+			$this->view->variables()->set('sSpath',$sLangCountry);
+			$arrLangSelectMenu = $this->getLangSelectMenu();
+			$this->view->variables()->set('arrLangSelectMenu',$arrLangSelectMenu);
+			$this->view->variables()->set('arrLangTranslation',$arrLangTranslationNew);
+		}
+		
+		
+		
+// 		$arrLangCountry = array();
+// 		$arrLangCountry = explode('_',$sSearchLangCountry);
+// 		if(count($arrLangCountry)!=2)
+// 		{
+// 			$skey = '请选择一个默认语言';
+// 			$this->createMessage(Message::error,"%s ",$skey) ;
+// 			$arrLangSelectMenu = $this->getLangSelectMenu();
+// 			$this->view->variables()->set('arrLangSelectMenu',$arrLangSelectMenu);
+// 			return;
+// 		}
+	
+// 		$aLocale = Locale::flyweight($arrLangCountry[0], $arrLangCountry[1]);
+// 		$sLangCountry = $aLocale->language().'_'.$aLocale->country();
+// 		$arrSentenceLibrary = $this->getSelectSentenceLibrary($sLangCountry);
+// 		$arrLangTranslationSelect = $this->setSelectSentenceLibraryPage(null,$arrSentenceLibrary);
+// 		$this->view->variables()->set('hidelangcountry', $this->params['sSwichFrontLangPath']);
+	
+// 		if(count($arrLangTranslationSelect)==0)
+// 		{
+// 			$bFlag = false;
+// 			$this->view->variables()->set('bFlag',$bFlag);
+	
+// 			$this->view->variables()->set('sSpath',$sLangCountry);
+// 			$arrLangSelectMenu = $this->getLangSelectMenu();
+// 			$this->view->variables()->set('arrLangSelectMenu',$arrLangSelectMenu);
+	
+// 		}else{
+	
+// 			$bFlag = true;
+// 			$this->view->variables()->set('bFlag',$bFlag);
+	
+// 			$arrLangTranslationChunk = array();
+// 			$arrLangTranslationChunk = $this->getLangChunk($arrLangTranslationSelect,$nPerPageRowNumber=20);
+// 			$arrLangTranslationNew = $this->getSelectSentenceLibraryNew($sLangCountry, $arrLangTranslationChunk,1);
+	
+	
+// 			$this->setPaginator($arrSentenceLibrary, $sLangCountry);
+	
+// 			$this->view->variables()->set('sSpath',$sLangCountry);
+// 			$arrLangSelectMenu = $this->getLangSelectMenu();
+// 			$this->view->variables()->set('arrLangSelectMenu',$arrLangSelectMenu);
+// 			$this->view->variables()->set('arrLangTranslation',$arrLangTranslationNew);
+// 		}
+	
+		
+		//选择语言
+		if($this->params['type'])
+		{
+			$sSpath = $this->params['sSwichFrontLangPath'];
+			$this->langSwichFront($sSpath);
+			$this->view->variables()->set('hidelangcountry', $sSpath);
+		}
+	
+		//翻页
+		if($this->params['paginator'])
+		{
+			$iCurrentPageNum = $this->params['paginator'];
+			$sLangCountry = $this->params['sLangCountry'];
+			$sSearchKey = $this->params['searchkey'];
+			
+			$arrSentenceLibrary = $this->getSelectSentenceLibrary($sLangCountry);
+			$arrSentenceLibrarySeach = $this->searchKey($arrSentenceLibrary,$sSearchKey);
+			$arrLangTranslationSelect = $this->setSelectSentenceLibraryPage('', $arrSentenceLibrarySeach);
+			$arrLangTranslationChunk = array();
+			$arrLangTranslationChunk = $this->getLangChunk($arrLangTranslationSelect,$nPerPageRowNumber=20);
+			$arrLangTranslationNew = $this->getSelectSentenceLibraryNew($sSearchKey, $arrLangTranslationChunk,$iCurrentPageNum);
+			$this->view->variables()->set('hidelangcountry', $sLangCountry);
+			$this->view->variables()->set('arrLangTranslation',$arrLangTranslationNew);
+		}
+		
+		$this->doActions();
+	}
+	
+	public function form()
+	{
+		$arrSentenceBase = array();
+		$arrSentenceUi = array();
+		$sPathBaseLibrarySentence='';
+		$sPathUiLibrarySentence='';
+		$i=0;
+		$sLangCountry = $this->params['hidden_lang_country'];
+	
+		//创建Ui和Base的语言库对象
+		foreach($this->params['TranslationSentence'] as $key=>$value)
+		{
+			//只取循环一次的
+			if($i==0)
+			{
+				//Base
+				$arrLangType=explode('@',$key);
+				$arrLangCountry=explode('_',$arrLangType[0]);
+				$aLocale = new Locale($arrLangCountry[0],$arrLangCountry[1]) ;
+				$aSentenceBase=$aLocale->sentenceLibrary('base');
+				$sSentenceBasePkgFileName=$aSentenceBase->packageFilename();
+				$sPathBaseLibrarySentence = Extension::flyweight('localizer')->unarchiveSentenceFolder()->path().'/'.$sSentenceBasePkgFileName;
+				$arrSentenceBase = is_file($sPathBaseLibrarySentence) ? include $sPathBaseLibrarySentence : array() ;
+				$arrSentenceBase = is_array($arrSentenceBase) ? $arrSentenceBase : array();
+				
+	
+				//Ui
+				$aSentenceUi = $aLocale->sentenceLibrary('ui');
+				$sSentenceUiPkgFileName = $aSentenceUi->packageFilename();
+				$sPathUiLibrarySentence = Extension::flyweight('localizer')->unarchiveSentenceFolder()->path().'/'.$sSentenceUiPkgFileName;
+				$arrSentenceUi = is_file($sPathUiLibrarySentence) ? include $sPathUiLibrarySentence : array() ;
+				$arrSentenceUi = is_array($arrSentenceUi) ? $arrSentenceUi : array();
+			};
+			$i++;
+		}
+	
+		//将Ui和Base的语言库创建长数组
+		foreach($this->params['TranslationSentence'] as $key=>$value)
+		{
+			$arrLangType=explode('@',$key);
+			if($arrLangType[1]=='base')
+			{
+				$arrSentenceBase[$arrLangType[2]]=$value;
+			}else{
+				$arrSentenceUi[$arrLangType[2]] = $value;
+			}
+		};
+		file_put_contents($sPathBaseLibrarySentence,'<?php return'.' '.var_export($arrSentenceBase,true).';');
+		file_put_contents($sPathUiLibrarySentence,'<?php return'.' '.var_export($arrSentenceUi,true).';');
+		$sTranslationFilePath = UIFactory::singleton()->sourceFileManager()->compiledFolderPath();
+		
+		
+		$this->deldir($sTranslationFilePath);
+		$this->createMessage(Message::success,"%s ",$skey="保存成功") ;
+		$this->location('?c=org.opencomb.localizer.LangTranslation&sSwichFrontLangPath='.$sLangCountry);
+		
+		//rmdir($sTranslationFilePath);
+	}
+
+	//获得选择语言列表
+	public function getLangSelectMenu()
+	{
+		$aSetting = Extension::flyweight('localizer')->setting();
+		$aKey = $aSetting->key('/localizer',true);
+		foreach($aKey->itemIterator() as $key=>$value){
+			$arrLangSelectMenu[$value]=$aKey->item($value,array());
+		}
+		return $arrLangSelectMenu;
+	}
+
+	//获得语言包数组
+	public function getSelectSentenceLibrary($sSpath)
+	{
+		$arrSpath = explode('_',$sSpath);
+		$aLocale = new Locale($arrSpath[0],$arrSpath[1]) ;
+		$aSentenceBase = $aLocale->sentenceLibrary('base');
+		$sSentenceBasePkgFileName = $aSentenceBase->packageFilename();
+		$sPathBaseLibrarySentence = Extension::flyweight('localizer')->unarchiveSentenceFolder()->path().'/'.$sSentenceBasePkgFileName;
+		$arrSentenceBase = array();
+		$arrSentenceUi = array();
+		$arrSentenceLibrary = array();
+
+		if(file_exists($sPathBaseLibrarySentence))
+		{	
+			$arrSentenceBase = include $sPathBaseLibrarySentence;
+		}
+
+		$aSentenceUi = $aLocale->sentenceLibrary('ui');
+		$sSentenceUiPkgFileName = $aSentenceUi->packageFilename();
+		$sPathUiLibrarySentence = Extension::flyweight('localizer')->unarchiveSentenceFolder()->path().'/'.$sSentenceUiPkgFileName;
+		if(file_exists($sPathUiLibrarySentence))
+		{	
+			$arrSentenceUi = include $sPathUiLibrarySentence;
+		}
+
+		if(!is_array($arrSentenceBase))
+		{
+			$arrSentenceBase = array();
+		}
+
+			if(!is_array($arrSentenceUi))
+		{
+			$arrSentenceUi = array();
+		}
+
+		$arrSentenceLibrary['base'] = $arrSentenceBase;
+		$arrSentenceLibrary['ui'] = $arrSentenceUi;
+
+		return $arrSentenceLibrary;
+	}
+
+	//获得分页内容
+	public function getSelectSentenceLibraryNew($sSpath, $arrLangTranslationChunk,$iCurrentPageNum=1)
+	{
+		foreach($arrLangTranslationChunk[$iCurrentPageNum-1] as $key=>$value)
+		{
+			$arrLangTranslationNew[$sSpath][$value['type']][$value['hash']]=$value['value'];
+		}
+		return $arrLangTranslationNew;
+	}
+
+
+
+	public function setSelectSentenceLibraryPage($sSpath,$arrSentenceLibrary)
+	{
+		$arrLangTranslationSelect = array();
+		foreach($arrSentenceLibrary['base'] as $keyHash=>$value)
+		{
+			$arrLangTranslationSelect[]=array('type'=>'base','hash'=>$keyHash,'value'=>$value);
+		}
+
+		foreach($arrSentenceLibrary['ui'] as $keyHash=>$value)
+		{
+			$arrLangTranslationSelect[]=array('type'=>'ui','hash'=>$keyHash,'value'=>$value);
+		}
+
+		return $arrLangTranslationSelect;
+	}
+
+	//
+	public function getLangChunk($arrLangTranslationSelect,$nPerPageRowNumber=20)
+	{
+		return array_chunk($arrLangTranslationSelect,$nPerPageRowNumber);
+	}
+
+	//切换前台语言
+	public function langSwichFront($sSpath)
+	{
+
+		$sLangCountry = $sSpath;
+		$arrLangCountry = explode('_',$sSpath);
+		$aLocale = new Locale($arrLangCountry[0],$arrLangCountry[1]);
+		$arrSentenceLibrary = $this->getSelectSentenceLibrary($sLangCountry);
+		$arrLangTranslationSelect = $this->setSelectSentenceLibraryPage(null,$arrSentenceLibrary);
+		if(count($arrLangTranslationSelect)==0)
+		{	
+				$bFlag = false;
+				$this->view->variables()->set('bFlag',$bFlag);
+
+				$this->setPaginator($arrSentenceLibrary, $sLangCountry);
+
+				$this->view->variables()->set('sLangCountry',$sLangCountry);
+				$this->view->variables()->set('sSpath',$sLangCountry);
+				$arrLangSelectMenu = $this->getLangSelectMenu();
+				$this->view->variables()->set('arrLangSelectMenu',$arrLangSelectMenu);
+
+		}else{
+
+			$bFlag = true;
+			$this->view->variables()->set('bFlag',$bFlag);
+
+			$arrLangTranslationChunk = array();
+			$arrLangTranslationChunk = $this->getLangChunk($arrLangTranslationSelect,$nPerPageRowNumber=20);
+			$arrLangTranslationNew = $this->getSelectSentenceLibraryNew($sLangCountry, $arrLangTranslationChunk,1);
+
+			$this->setPaginator($arrSentenceLibrary, $sLangCountry);
+
+			$this->view->variables()->set('sLangCountry',$sLangCountry);
+			$this->view->variables()->set('sSpath',$sLangCountry);
+			$arrLangSelectMenu = $this->getLangSelectMenu();
+			$this->view->variables()->set('arrLangSelectMenu',$arrLangSelectMenu);
+			$this->view->variables()->set('arrLangTranslation',$arrLangTranslationNew);
+
+		}
+
+	}
+	
+	public function searchKey($arrSentenceLibrary,$sSearchKey)
+	{
+		$arrSentenceLibrarySeach = array();
+		foreach($arrSentenceLibrary as $key=>$arrSentence)
+		{
+			foreach($arrSentence as $hash=>$content)
+			{
+				if(substr_count($content,$sSearchKey))
+				{
+					$arrSentenceLibrarySeach[$key][$hash] = $content;
+				}
+			}
+		}
+		
+		if(!array_key_exists('base',$arrSentenceLibrarySeach))
+		{
+			$arrSentenceLibrarySeach['base'] = array();
+		}
+		
+		if(!array_key_exists('ui',$arrSentenceLibrarySeach))
+		{
+			$arrSentenceLibrarySeach['ui'] = array();
+		}
+		return $arrSentenceLibrarySeach;
+	}
+
+
+	//设置分页器
+	public function setPaginator($arrSentenceLibrary,$sLangCountry)
+	{
+		$nTotal = 0;
+		$nTotal = count($arrSentenceLibrary['base']) + count($arrSentenceLibrary['ui']);
+		$nPerPageRowNumber = 20;
+
+		$this->view->widget('paginator')->setTotalCount($nTotal);
+		$this->view->widget('paginator')->setPerPageCount($nPerPageRowNumber);
+	}
+	
+	public function deldir($dir) {
+	
+		//先删除目录下的文件：
+	
+		$dh=opendir($dir);
+	
+		while ($file=readdir($dh)) {
+	
+			if($file!="." && $file!="..") {
+	
+				$fullpath=$dir."/".$file;
+	
+				if(!is_dir($fullpath)) {
+	
+					unlink($fullpath);
+	
+				} else {
+	
+					$this->deldir($fullpath);
+	
+				}
+	
+			}
+	
+		}
+	
+	
+		closedir($dh);
+	
+		//删除当前文件夹：
+	
+		if(rmdir($dir)) {
+	
+			return true;
+	
+		} else {
+	
+			return false;
+	
+		}
+	
+	}
+
+}
+
+?>
